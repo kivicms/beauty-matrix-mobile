f794a467bd53b7d6ee4360e4c9ae0b193974acee
kivicms <v.ivanov@orb.enforta.com>
2018-02-17 12:22:18 +0500

    First commit

---
+++ src/modules/training/training.vue
@@ -0,0 +1,23 @@
+<template>
+  <f7-page>
+    <f7-navbar title="Тренировки" back-link="Back"></f7-navbar>
+    <f7-block-title>Тренировки</f7-block-title>
+    <f7-block strong>
+      <p>Тут будут тренировки!</p>
+    </f7-block>
+  </f7-page>
+</template>
+<script>
+export default {
+  name: 'training',
+  data: function() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    console.log('on created')
+  }
+}
+</script>
+
